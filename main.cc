7292abbc0b900fdb52d1e0d8556acb3f8505a03b
jhzHP <harold.jiang@outlook.com>
2018-07-22 16:06:21 -0700

    first edition

--- main.cc
+++ main.cc
@@ -1,38 +1,74 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 class MyCircularQueue {
+private:
+    int num;
+    int c;
+    vector<int> data;
+    int s,e;
 public:
     /** Initialize your data structure here. Set the size of the queue to be k. */
     MyCircularQueue(int k) {
-        
+        if (k>0)
+            data=vector<int> (k);
+        num=k;
+        s=0;e=0;c=0;
     }
     
     /** Insert an element into the circular queue. Return true if the operation is successful. */
     bool enQueue(int value) {
-        
+        if (num-c>0){
+            e++;
+            if (e==num)
+                e=0;
+            data[e]=value;
+            c++;
+            return true;
+        }
+        else
+            return false;
     }
     
     /** Delete an element from the circular queue. Return true if the operation is successful. */
     bool deQueue() {
-        
+        if (c>0){
+            s++;
+            if (s==num)
+                s=0;
+            c--;
+            return true;
+        }
+        else
+            return false;
     }
     
     /** Get the front item from the queue. */
     int Front() {
-        
+        if (c>0)
+            return data[s];
+        else
+            return -1;
     }
     
     /** Get the last item from the queue. */
     int Rear() {
-        
+        if (c>0)
+            return data[e];
+        else
+            return -1;
     }
     
     /** Checks whether the circular queue is empty or not. */
     bool isEmpty() {
-        
+        return (c==0);
     }
     
     /** Checks whether the circular queue is full or not. */
     bool isFull() {
-        
+        return (num-c==0);
     }
 };
 
